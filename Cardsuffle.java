b4a1c69e628b6f3c808ea70e31f1cc5494db5d06
Jitendra Pratap <jitendrapratap432@gmail.com>
2024-11-18 23:36:34 +0530

    some change

--- Cardsuffle.java
+++ Cardsuffle.java
@@ -6,7 +6,6 @@ public class Cardsuffle {
 		int pickacard = input.nextInt();
 		String[] decks = {"clubs","diamond","hearts","spades"};
 		String[] cards = {"A","2","3","4","5","6","7","8","9","10","Q","K","J"};
-		
 		 int pickedDeck = (int)(Math.random() * decks.length);
 		 int pickedCard = (int)(Math.random() * cards.length);
 		System.out.println("your card is : " + pickedCard + " and Deck is " + decks[pickedDeck]);
