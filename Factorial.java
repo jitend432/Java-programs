585994f2cc8f6adbd1822cbcc841cf5a6d045bf5
Jitendra Pratap <jitendrapratap432@gmail.com>
2024-12-01 19:47:50 +0530

    New

--- Factorial.java
+++ Factorial.java
@@ -7,8 +7,7 @@ public class Factorial {
 	 int factorial = 1;
 	 for(int i = num; i>=1; i--) {
 		 factorial = factorial*i;
-	 	}
-		 
+		}		 
 		 System.out.println("Your Factorial is " +factorial);
 	}
 }
